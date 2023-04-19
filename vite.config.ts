6aeb25089451d3fd497b8a26b5690ce6fcac13b0
RipperZero <1018744707@qq.com>
2023-04-19 11:17:44 +0900

    feat: use https

--- vite.config.ts
+++ vite.config.ts
@@ -1,14 +1,21 @@
 import { defineConfig } from "vite";
 
 import react from "@vitejs/plugin-react";
+import mkcert from "vite-plugin-mkcert";
 import tsconfigPaths from "vite-tsconfig-paths";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react(), tsconfigPaths()],
+  plugins: [
+    react(),
+    tsconfigPaths(),
+    // @see https://github.com/liuweiGL/vite-plugin-mkcert
+    mkcert(),
+  ],
   server: {
     port: 3000,
     host: true,
     cors: true,
+    https: true,
   },
 });
