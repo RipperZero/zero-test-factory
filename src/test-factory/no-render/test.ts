f5a1d2716245bd388671d5c7d0ebab787bc5634e
RipperZero <1018744707@qq.com>
2023-04-27 18:12:10 +0900

    faet: add validator

--- src/test-factory/no-render/test.ts
+++ src/test-factory/no-render/test.ts
@@ -1,31 +1,23 @@
-export {};
-
 // interface A {
 //   a: number;
 //   aa: string;
 //   aaa: boolean;
 // }
-
 // interface B {
 //   b: undefined;
 // }
-
 // interface C extends A, B {
 //   c: null;
 // }
-
 // type A = {
 //   a: number;
 //   aa: string;
 //   aaa: boolean;
 // };
-
 // type B = {
 //   b: undefined;
 // };
-
 // type C = A & B & { c: null };
-
 // const c: C = {
 //   a: 0,
 //   aa: "aa",
@@ -33,9 +25,7 @@ export {};
 //   b: undefined,
 //   c: null,
 // };
-
 // console.log(c);
-
 // class ZeroA {
 //   private static aFunc = () => {
 //     return "aaa";
@@ -44,7 +34,6 @@ export {};
 // ZeroA.aFunc();
 // // @ts-ignore:next-line
 // ZeroA.aFunc();
-
 // class ZeroB {
 //   public bFunc = () => {
 //     return "bbb";
@@ -52,3 +41,11 @@ export {};
 // }
 // const objB = new ZeroB().bFunc;
 // objB();
+import isEmail from "validator/lib/isEmail";
+import isMobilePhone from "validator/lib/isMobilePhone";
+
+const email = "123546@qq.com";
+const mobilePhone = "15249203158";
+
+console.log(isEmail(email));
+console.log(isMobilePhone(mobilePhone, "zh-CN"));
