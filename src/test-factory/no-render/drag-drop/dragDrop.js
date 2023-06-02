a5bc912ffa204a0a50a6611899e49ee35fa7358f
RipperZero <1018744707@qq.com>
2023-06-02 18:10:38 +0900

    feat: update all packages version

--- src/test-factory/no-render/drag-drop/dragDrop.js
+++ src/test-factory/no-render/drag-drop/dragDrop.js
@@ -9,13 +9,15 @@ source.addEventListener("dragstart", (event) => {
 const target = document.getElementById("droptarget");
 target.addEventListener("dragover", (event) => {
   // prevent default to allow drop
-  // @see https://developer.mozilla.org/zh-CN/docs/Web/API/HTML_Drag_and_Drop_API/Drag_operations#%E6%8C%87%E5%AE%9A%E6%94%BE%E7%BD%AE%E7%9B%AE%E6%A0%87
+  // @see
+  // https://developer.mozilla.org/zh-CN/docs/Web/API/HTML_Drag_and_Drop_API/Drag_operations#%E6%8C%87%E5%AE%9A%E6%94%BE%E7%BD%AE%E7%9B%AE%E6%A0%87
   event.preventDefault();
 });
 
 target.addEventListener("drop", (event) => {
   // prevent default action (open as link for some elements)
-  // @see https://developer.mozilla.org/zh-CN/docs/Web/API/HTML_Drag_and_Drop_API/Drag_operations#%E6%89%A7%E8%A1%8C%E6%94%BE%E7%BD%AE
+  // @see
+  // https://developer.mozilla.org/zh-CN/docs/Web/API/HTML_Drag_and_Drop_API/Drag_operations#%E6%89%A7%E8%A1%8C%E6%94%BE%E7%BD%AE
   event.preventDefault();
   // move dragged element to the selected drop target
   if (event.target.className === "dropzone") {
